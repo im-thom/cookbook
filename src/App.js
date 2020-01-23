b7aeba7771477457698fc8d89c52609945682df4
Sean Dorr <sdorr@gemmas-imac.local>
2020-01-23 19:09:44 +0000

    Stylizing recipe card component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import RecipeCard from './components/RecipeCard';
 
 const App = () => {
 	return (
-		<div>
-			<RecipeCard />
+		<div className="container">
+			<div className="recipe-grid">
+				<RecipeCard
+					recipeName="Recipe Name"
+					recipeImg={"/images/food.jpg"}
+					recipeCookTime={"10min."}
+					recipeRating={"****"}
+				/>
+				<RecipeCard
+					recipeName="Another Recipe"
+					recipeImg={"/images/food.jpg"}
+					recipeCookTime={"45min."}
+					recipeRating={"*****"}
+				/>
+			</div>
 		</div>
 	);
 };
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const RecipeCard = () => {
+const RecipeCard = (props) => {
 	return (
-		<div>
-			Recipe Card
-		</div>
+		<a className="card recipe-card">
+			<div className="col-container">
+				<div className="col">
+					<img className="img card recipe-img" src={props.recipeImg} />
+				</div>
+				<div className="col" id="recipe-content">
+					<h2 className="recipe-name">{props.recipeName}</h2>
+					<div className="recipe-info">
+						<p>{props.recipeCookTime}</p>
+						<p>{props.recipeRating}</p>
+					</div>
+				</div>
+			</div>
+		</a>
 	);
 };
 
