505e90df0badc15c04dad35c1a58c93f615e27b9
Sean Dorr <sdorr@gemmas-imac.local>
2020-01-30 17:52:05 +0000

    Reconfiguring recipes to pull data through arrays

--- src/App.js
+++ src/App.js
@@ -4,43 +4,73 @@ import RecipeCard from './components/RecipeCard';
 import FilterTag from './components/FilterTag';
 
 const App = () => {
+	const recipeTiles = recipes.map((recipe, i) => {
+		const tagPills = recipe.tags.cuisine.map((tag, i)  => {
+			console.log("here's I", i);
+			return (
+				<span key={i}>
+					<FilterTag
+						tagName={tag}
+					/>
+				</span>
+			);
+		});
+		
+		return(
+			<RecipeCard key={i}
+				recipeName={recipe.recipeName}
+				recipeImg={recipe.recipeImg}
+				recipeCookTime={recipe.recipeCookTime}
+				recipeRating={recipe.recipeRating}
+				recipeLink={recipe.recipeLink}
+			>
+				{tagPills}
+			</RecipeCard>
+		);
+	});
+	
 	return (
 		<div className="container">
 			<SearchBar 
 				accountImage={"../images/account.jpg"}
 			/>
 			<div className="recipe-grid">
-				<RecipeCard
-					recipeName="Recipe Name"
-					recipeImg={"/images/food.jpg"}
-					recipeCookTime={"10min."}
-					recipeRating={"****"}
-					recipeLink={"#"}
-				/>
-				<RecipeCard
-					recipeName="Another Recipe"
-					recipeImg={"/images/food.jpg"}
-					recipeCookTime={"45min."}
-					recipeRating={"*****"}
-					recipeLink={"#"}
-				/>
-				<RecipeCard
-					recipeName="Recipe Name"
-					recipeImg={"/images/food.jpg"}
-					recipeCookTime={"10min."}
-					recipeRating={"****"}
-					recipeLink={"#"}
-				/>
-				<RecipeCard
-					recipeName="Another Recipe"
-					recipeImg={"/images/food.jpg"}
-					recipeCookTime={"45min."}
-					recipeRating={"*****"}
-					recipeLink={"#"}
-				/>
+				{recipeTiles}
 			</div>
 		</div>
 	);
 };
+
+const recipes = [
+	{
+		recipeName: "Recipe Name",
+		recipeImg: "/images/food.jpg",
+		recipeCookTime: 40,
+		recipeRating: "****",
+		recipeLink: "#",
+		tags: {
+			cuisine: [
+				"amer.",
+			],
+			expense: 1,
+		}
+	}, 
+	
+	{
+		recipeName: "Another Name",
+		recipeImg: "/images/food.jpg",
+		recipeCookTime: 10,
+		recipeRating: "****",
+		recipeLink: "#",
+		tags: {
+			cuisine: [
+				"amer.",
+				"korean",
+				"italian"
+			],
+			expense: 1,
+		}
+	},
+];
 
 export default App;
