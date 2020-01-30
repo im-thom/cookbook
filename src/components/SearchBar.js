21ad3268bfde5f442b1214ab9de509e708b586ec
Sean Dorr <sdorr@gemmas-imac.local>
2020-01-30 17:53:51 +0000

    Adding in toggle event to hide / show menu

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-	state = { term: 'Search...' };
+	state = { 
+		term: 'Search...',
+		menuIsShowing: false
+	};
+	
+	componentDidMount() {
+		
+	}
 	
 	onFormSubmit = (event) => {
 		event.preventDefault();
@@ -9,14 +16,25 @@ class SearchBar extends React.Component {
 		this.props.onSubmit(this.state.term);
 	}
 	
+	toggleMenu() {
+		this.setState({menuIsShowing: !this.state.menuIsShowing})
+	}
+	
 	render() {
 		return (
-			<form onSubmit={this.onFormSubmit} className="search-form">
-				<input className="card search-bar" type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })}/>
-				<div className="card account">
-					<img className="img account-img" src={this.props.accountImage} />
-				</div>
-			</form>
+			<>
+				<form onSubmit={this.onFormSubmit} className="search-form">
+					<input className="card search-bar" type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })}/>
+					<div className="card account">
+						<img className="img account-img" src={this.props.accountImage} onClick={() => this.toggleMenu()} />
+					</div>
+				</form>
+				{this.state.menuIsShowing && (
+					<div>
+						not open
+					</div>
+				)}
+			</>
 		);
 	}
 }
