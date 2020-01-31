8511575a16b83388501982c8303982a3c87c9b82
Sean Dorr <sdorr@gemmas-imac.local>
2020-01-31 13:38:28 +0000

    Removing navbar from searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,42 +1,18 @@
 import React from 'react';
+// import NavBarContainer from '';
 
-class SearchBar extends React.Component {
-	state = { 
-		term: 'Search...',
-		menuIsShowing: false
-	};
+const SearchBar = (props) =>  {
 	
-	componentDidMount() {
-		
-	}
-	
-	onFormSubmit = (event) => {
-		event.preventDefault();
-		
-		this.props.onSubmit(this.state.term);
-	}
-	
-	toggleMenu() {
-		this.setState({menuIsShowing: !this.state.menuIsShowing})
-	}
-	
-	render() {
-		return (
-			<>
-				<form onSubmit={this.onFormSubmit} className="search-form">
-					<input className="card search-bar" type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })}/>
-					<div className="card account">
-						<img className="img account-img" src={this.props.accountImage} onClick={() => this.toggleMenu()} />
-					</div>
-				</form>
-				{this.state.menuIsShowing && (
-					<div>
-						not open
-					</div>
-				)}
-			</>
-		);
-	}
+	return (
+		<>
+			<form className="search-form">
+				<input className="card search-bar" type="text" placeholder={props.placeholder} value={props.term} onChange={e => props.handleChange(e.target.value )}/>
+				<div className="card account">
+					<img className="img account-img" src={props.accountImage} onClick={() => props.toggleMenu()} />
+				</div>
+			</form>
+		</>
+	);
 }
 
 export default SearchBar;
