2ac6bcaa9d55a7b66c8ebc874270ac28f6f5ae1a
Sean Dorr <sdorr@gemmas-imac.local>
2020-01-31 13:37:35 +0000

    Restructuring state for menu toggle

--- src/App.js
+++ src/App.js
@@ -3,42 +3,63 @@ import SearchBar from './components/SearchBar';
 import RecipeCard from './components/RecipeCard';
 import FilterTag from './components/FilterTag';
 
-const App = () => {
-	const recipeTiles = recipes.map((recipe, i) => {
-		const tagPills = recipe.tags.cuisine.map((tag, i)  => {
-			console.log("here's I", i);
-			return (
-				<span key={i}>
-					<FilterTag
-						tagName={tag}
-					/>
-				</span>
+class App extends React.Component {
+	state = { menuIsOpen: false }
+	
+	toggleMenu() {
+		this.setState({menuIsOpen: !this.state.menuIsOpen})
+	}
+	
+	handleChange(value) {
+		this.setState({term: value})
+	}
+	
+	render () {
+		const recipeTiles = recipes.map((recipe, i) => {
+			const tagPills = recipe.tags.cuisine.map((tag, i)  => {
+				return (
+					<span key={i}>
+						<FilterTag
+							tagName={tag}
+						/>
+					</span>
+				);
+			});
+			
+			return(
+				<RecipeCard key={i}
+					recipeName={recipe.recipeName}
+					recipeImg={recipe.recipeImg}
+					recipeCookTime={recipe.recipeCookTime}
+					recipeRating={recipe.recipeRating}
+					recipeLink={recipe.recipeLink}
+				>
+					{tagPills}
+				</RecipeCard>
 			);
 		});
 		
-		return(
-			<RecipeCard key={i}
-				recipeName={recipe.recipeName}
-				recipeImg={recipe.recipeImg}
-				recipeCookTime={recipe.recipeCookTime}
-				recipeRating={recipe.recipeRating}
-				recipeLink={recipe.recipeLink}
-			>
-				{tagPills}
-			</RecipeCard>
+		return (
+			<>
+				<div className="container">
+					<SearchBar 
+						term={this.state.term}
+						handleChange={(value) => this.handleChange(value)}
+						toggleMenu={() => this.toggleMenu()}
+						accountImage={"../images/account.jpg"}
+					/>
+					<div className="recipe-grid">
+						{recipeTiles}
+					</div>
+				</div>
+				{this.state.menuIsOpen && 
+					<div className="menu-overlay">
+						menu
+					</div>
+				}
+			</>
 		);
-	});
-	
-	return (
-		<div className="container">
-			<SearchBar 
-				accountImage={"../images/account.jpg"}
-			/>
-			<div className="recipe-grid">
-				{recipeTiles}
-			</div>
-		</div>
-	);
+	};
 };
 
 const recipes = [
@@ -55,6 +76,38 @@ const recipes = [
 			expense: 1,
 		}
 	}, 
+	
+	{
+		recipeName: "Another Name",
+		recipeImg: "/images/food.jpg",
+		recipeCookTime: 10,
+		recipeRating: "****",
+		recipeLink: "#",
+		tags: {
+			cuisine: [
+				"amer.",
+				"korean",
+				"italian"
+			],
+			expense: 1,
+		}
+	},
+	
+	{
+		recipeName: "Another Name",
+		recipeImg: "/images/food.jpg",
+		recipeCookTime: 10,
+		recipeRating: "****",
+		recipeLink: "#",
+		tags: {
+			cuisine: [
+				"amer.",
+				"korean",
+				"italian"
+			],
+			expense: 1,
+		}
+	},
 	
 	{
 		recipeName: "Another Name",
