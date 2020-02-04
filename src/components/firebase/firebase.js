b9b5330e0649713b00da8c92d7d148ce15bf1726
Thom Wheeler <wheeler2501@gmail.com>
2020-02-04 16:34:34 +0100

    Removed api key added env vars

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 // import React, { Component } from "react";
 
 const config = {
-  apiKey: "AIzaSyASb4yZ0JY3CLvlkONZI7k3CzvcVNr8PN0",
-  authDomain: "cookbook-f485f.firebaseapp.com",
-  databaseURL: "https://cookbook-f485f.firebaseio.com",
-  projectId: "cookbook-f485f",
-  storageBucket: "cookbook-f485f.appspot.com",
-  messagingSenderId: "910034597723",
-  appId: "1:910034597723:web:41b79fe114974e5c80cc7e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 class Firebase {
