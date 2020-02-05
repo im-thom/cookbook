c694ff228d120330a53b7aac505a2884846cb307
Thom Wheeler <wheeler2501@gmail.com>
2020-02-05 17:13:41 +0100

    Auth state based navbar

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,14 +1,17 @@
-import React from react;
+import React from "react";
 import NavBar from "./NavBar";
+import AuthUserContext from "../session/context";
 
+const NavBarWithAuth = () => {
+  return <div className="navbar-container">navbar</div>;
+};
 
+const NavbarNoAuth = () => <div className="navbar-container">navbar WITHOUT AUTH</div>;
 
-const NavBarContainer = (props) => {
-	return (
-		<div className="navbar-container">
-			navbar
-		</div>
-	);
-}
+const Navbar = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (authUser ? <NavBarWithAuth /> : <NavbarNoAuth />)}
+  </AuthUserContext.Consumer>
+);
 
-export default NavBarContainer;
+export default Navbar;
