4f7e74f48a47a3d4a84f089d9cfb5b1ec2ebd107
Thom Wheeler <wheeler2501@gmail.com>
2020-02-05 17:12:23 +0100

    Add cors to firestore functions

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,56 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 let db = admin.firestore();
 
-
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!");
 });
 
 exports.get_recipes = functions.https.onRequest(async (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
+  res.set("Access-Control-Allow-Origin", "*");
 
+  if (req.method === "OPTIONS") {
+    // Send response to OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+  } else {
     try {
-        // Create a snapshot with the entire recipes collection
-        const snapshot = await db.collection('recipes').get();
-        // Empyt array to hold recipes
-        const recipes = [];
-        // Loop through each document and push data to the empty recipes array
-        snapshot.forEach(recipe => {
-            recipes.push(recipe.data())
-        });
-        // Send the recipes array in a response object
-        res.send(recipes);
-
+      // Create a snapshot with the entire recipes collection
+      const snapshot = await db.collection("recipes").get();
+      // Empyt array to hold recipes
+      const recipes = [];
+      // Loop through each document and push data to the empty recipes array
+      snapshot.forEach(recipe => {
+        recipes.push(recipe.data());
+      });
+      // Send the recipes array in a response object
+      res.send(recipes);
     } catch (error) {
-        res.send(error)
+      res.send(error);
     }
-})
+  }
+});
 
+// cors(async (req, res) => {
+
+// })
 
 exports.create_new_recipe = functions.https.onRequest(async (req, res) => {
-    try {
-        let setDoc = await db.collection('recipes').add({
-            name: req.body.name
-        });
-        res.send(`Successfully added ${req.body.name} to database.`)
-    } catch (error) {
-        res.send(`Unable to add ${req.body.name} to database: ${error}`)
-    }
-})
+  try {
+    let setDoc = await db.collection("recipes").add({
+      name: req.body.name
+    });
+    res.send(`Successfully added ${req.body.name} to database.`);
+  } catch (error) {
+    res.send(`Unable to add ${req.body.name} to database: ${error}`);
+  }
+});
