9edb1b232e8e34e68f9b19c84e770fa96d3b630e
Thom Wheeler <wheeler2501@gmail.com>
2021-05-20 10:27:51 +0100

    FIXES: recipe resolver path

--- server/src/resolvers/Recipe.ts
+++ server/src/resolvers/Recipe.ts
@@ -1,4 +1,4 @@
-import { Recipe } from "src/entities/Recipe";
+import { Recipe } from "../entities/Recipe";
 import { Resolver, Query, Ctx } from "type-graphql";
 import { MyContext } from "../types";
 
