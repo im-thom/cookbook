80a4f643bc1df6918cbb1b16b620c56773881f47
Thom Wheeler <wheeler2501@gmail.com>
2021-05-21 15:49:28 +0100

    UPDATED: resolvers to use typeorm

--- server/src/resolvers/Recipe.ts
+++ server/src/resolvers/Recipe.ts
@@ -1,11 +1,55 @@
 import { Recipe } from "../entities/Recipe";
-import { Resolver, Query, Ctx } from "type-graphql";
-import { MyContext } from "../types";
+import {
+  Resolver,
+  Query,
+  Arg,
+  Mutation,
+  Int,
+  InputType,
+  Field,
+} from "type-graphql";
 
-@Resolver()
+@InputType()
+class RecipeUpdateInput {
+  @Field(() => String, { nullable: true })
+  title?: string;
+}
+
+@Resolver(Recipe)
 export class RecipeResolver {
+  // Read all recipes
   @Query(() => [Recipe])
-  getRecipes(@Ctx() {}: MyContext) {
-    return [];
+  recipes() {
+    return Recipe.find({});
+  }
+
+  // Read single Recipe by id
+  @Query(() => Recipe)
+  recipe(@Arg("id", () => Int) id: number) {
+    return Recipe.findOne(id, {});
+  }
+
+  // Create Recipe
+  @Mutation(() => Recipe)
+  async createRecipe(@Arg("title") title: string) {
+    const recipe = await Recipe.create({ title }).save();
+    return recipe;
+  }
+
+  // Update Recipe
+  @Mutation(() => Recipe)
+  async updateRecipe(
+    @Arg("id", () => Int) id: number,
+    @Arg("updates", () => RecipeUpdateInput) updates: RecipeUpdateInput
+  ) {
+    await Recipe.update({ id }, updates);
+    return Recipe.findOne({ id });
+  }
+
+  // Delete Recipe
+  @Mutation(() => Boolean)
+  async deleteRecipe(@Arg("id", () => Int) id: number) {
+    await Recipe.delete({ id });
+    return true;
   }
 }
