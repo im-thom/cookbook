ce57964d5ed47262c06d87062b56693b30aa196a
Thom Wheeler <wheeler2501@gmail.com>
2021-05-24 17:00:56 +0100

    ADDED: integrated recipe loader and adds redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,11 @@ import { Recipe } from "./entities/Recipe";
 import { RecipeResolver } from "./resolvers/Recipe";
 import { createConnection } from "typeorm";
 import path from "path";
+import createRecipeLoader from "./loaders/Recipe";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { __prod__ } from "./constants";
 
 const PORT = process.env.PORT || 3001;
 
@@ -23,13 +28,37 @@ const PORT = process.env.PORT || 3001;
 
   const app = express();
 
+  // Connect redis middleware before Apollo - ordering important
+  const RedisStore = connectRedis(session);
+  const redis = new Redis(process.env.REDIS_URL);
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redis, disableTouch: true }),
+      // Hide this later...
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__,
+      },
+      secret: "ergwergvere",
+      resave: false,
+    })
+  );
+
   // Setup apollo server
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [RecipeResolver],
       validate: false,
     }),
-    context: ({ req, res }) => ({ req, res }),
+    context: ({ req, res }) => ({
+      req,
+      res,
+      redis,
+      recipeLoader: createRecipeLoader(),
+    }),
   });
 
   apolloServer.applyMiddleware({ app });
